312482774e030346467f90c1d0560cffe6e4e4ef
avialeks <101255331+avialeks@users.noreply.github.com>
2023-03-02 16:29:40 +0700

    Add files via upload

---
+++ CRYPTO_BOT/config.py
@@ -0,0 +1,16 @@
+TOKEN = '6119666715:AAFpgDKrLfifBA4fF5WHPpJQREpM0GWJS4g'
+keys = {
+    'BTC': 'BTC',
+    'ETH': 'ETH',
+    'USD': 'USD',
+    'EUR': 'EUR',
+    'RUB': 'RUB',
+}
+
+keys2 = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+}
---
+++ CRYPTO_BOT/extensions.py
@@ -0,0 +1,34 @@
+import requests
+import json
+from config import keys
+
+
+class APIException(Exception):
+    pass
+
+
+class CryptoConverter:
+    @staticmethod
+    def convert(quote: str, base: str, amount: str):
+        if quote == base:
+            raise APIException(f'Невозможно перевести одинаковые валюты "{base}".\nСписок всех доступных валют: /values')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f'"{quote}" такой валюты нет в списке.\nСписок всех доступных валют: /values')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f'"{base}" такой валюты нет в списке.\nСписок всех доступных валют: /values')
+
+        try:
+            amount = float(amount.replace(",", "."))
+        except KeyError:
+            raise APIException(f'Не удалось обработать колличество "{amount}"')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[keys[base]]
+
+        return total_base
